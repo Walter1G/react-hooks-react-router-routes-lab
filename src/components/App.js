cee85bf1ed6601ff2baf958e711cd231173248d2
Walter N <2760walter@gmail.com>
2023-11-03 10:23:23 +0300

    Add Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Routes, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
@@ -7,7 +7,20 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+
+  console.log("app")
+  return <div>
+    <NavBar />
+
+    <Switch>
+      <Route path="/" element={<Home />} />
+      <Route path="/directors" element={<Directors />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/actors" element={<Actors />} />
+    </Switch>
+
+
+  </div>;
 }
 
 export default App;
