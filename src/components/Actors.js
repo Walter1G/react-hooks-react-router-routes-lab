338041e9a976a2790df5964532a1aa6ce87ae571
Walter N <2760walter@gmail.com>
2023-11-03 11:52:14 +0300

    dsiplay Actors

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { actors } from "../data";
+// import { act } from "react-dom/test-utils";
 
 function Actors() {
+  function handleDisplayActors() {
+
+    return actors.map(actor => (
+      <div>
+        <h2>Name: {actor.name}</h2>
+        <p>Movies: </p>
+        <ul>
+          {actor.movies.map((movie, index) => <li key={index}>{movie}</li>)}
+        </ul>
+      </div>
+    ))
+
+
+  }
+
   return <div>
     <h1>Actors Page</h1>
-    {/*{code here}*/}</div>;
+    {handleDisplayActors()}
+  </div>;
 }
 
 export default Actors;
